4b6bdcd601dd5fad2f0914fb0bc3903593c19937
mananmpatel <mananmpatel2001@gmail.com>
2023-04-18 18:32:28 +0530

    Added startup Page with controller

---
+++ EventApplicationCore/Controllers/HomeNewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventApplicationCore.Controllers
+{
+    public class HomeNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
